ea49bcbec656ac312ae3d1e8121d9d0f148833bd
Wiktor Pink <wiktor.pink99@gmail.com>
2020-09-29 18:47:02 +0200

    added layout

--- app/src/main/java/com/example/coderswag/Contoller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Contoller/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.coderswag.Contoller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.coderswag.Adapters.CategoryAdapter
+import com.example.coderswag.Model.Category
 import com.example.coderswag.R
+import com.example.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : CategoryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = CategoryAdapter(this, DataService.categoties)
+
+
+
+        categoryListView.adapter = adapter
     }
 }
